90781e1f0140d461b2053ddd7a710e2ea0c51ca3
Williams <tkw10@netlab-11.cs.pitt.edu>
2017-09-24 21:08:53 -0400

    untested but mostly implemented http_server1

--- http_server1.cc
+++ http_server1.cc
@@ -6,6 +6,7 @@
 
 #define BUFSIZE 1024
 #define FILENAMESIZE 100
+#define BACKLOG 5 //Incoming connection queue size
 
 int handle_connection(int sock);
 
@@ -28,18 +29,30 @@ int main(int argc, char * argv[]) {
     }
 
     /* initialize and make socket */
-
+    int mi = minet_init(MINET_KERNEL); 
+    sock = minet_socket(SOCK_STREAM);
+    
     /* set server address*/
+    struct sockaddr_in ip4addr;
+    ip4addr.sin_family = AF_INET;
+    ip4addr.sin_port = server_port;
+    inet_pton(AF_INET, "10.0.0.1", &ip4addr.sin_addr);
 
     /* bind listening socket */
+    minet_bind(sock, &ip4addr);
 
     /* start listening */
+    minet_listen(sock, BACKLOG);
 
     /* connection handling loop: wait to accept connection */
-
+    int connfd;
+    struct sockaddr_in addrs;
     while (1) {
-	/* handle connections */
-	rc = handle_connection(sock);
+	connfd = minet_accept(sock, &addrs);
+	if(connfd != -1){
+	    /* handle connections */
+	    rc = handle_connection(connfd);
+	}
     }
 }
 
@@ -57,24 +70,71 @@ int handle_connection(int sock) {
 	"</body></html>\n";
     
     /* first read loop -- get request and headers*/
+    char buffer[BUFSIZE];
+    memset(buffer, '\0', BUFSIZE);
+    //A second buffer to handle telnet and other high latency connections
+    char b_buffer[BUFSIZE];
+    memset(buffer, '\0', BUFSIZE);
     
+    //minet_read returning 0 -> socket closed
+    while(minet_read(sock, buffer, BUFSIZE) != 0){
+	strcat(b_buffer, buffer);
+	memset(buffer, '\0', BUFSIZE);
+    }
     /* parse request to get file name */
     /* Assumption: this is a GET request and filename contains no spaces*/
-
+    char *str = strtok(buffer, " /\n");
+    char *file = NULL;
+    while(str != NULL){
+	if(!strcmp(str, "GET")){
+	    str = strtok(NULL, " \n");
+	    file = str;
+	    break; //For now we don't care about anything else
+	}
+    }
     /* try opening the file */
-
+    FILE *fp = fopen(str, "r");
+    long fsize;
+    char *contents = NULL;
+    if(fp){
+	if(fseek(fp, 0L, SEEK_END) == 0){
+	    fsize = ftell(fp);
+	    if(fsize == -1){
+		perror("Bad buffer size\n");
+		return NULL;
+	    }
+	    
+	    contents = malloc(sizeof(char) * (fsize + 1));
+	    if(fseek(fp, OL, SEEK_SET) != 0){
+		perror("Couldn't return to file start.\n");
+		return NULL;
+	    }
+	    
+	    if(ferror(fp) != 0){
+		perror("Error reading file");
+	    }
+	    else{
+		contents[fsize + 1] = '\0';
+	    }
+	    ok = 1;
+	}
+	fclose(fp);
+    }
     /* send response */
     if (ok) {
 	/* send headers */
-	
+	minet_write(sock, ok_response_f % fsize, strlen(ok_response_f)*sizeof(char));	
 	/* send file */
+	minet_write(sock, contents, fsize + 1);
 	
     } else {
 	// send error response
+	minet_write(sock, notok_response, strlen(notok_response)*sizeof(char));
     }
     
     /* close socket and free space */
-  
+    minet_close(sock);
+
     if (ok) {
 	return 0;
     } else {
