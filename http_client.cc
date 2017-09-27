6d3a38c30215906f173368c0aa10b2d2e2f3842c
Robert Colleran <rjc59@pitt.edu>
2017-09-27 00:46:56 -0400

    client error checking

--- http_client.cc
+++ http_client.cc
@@ -18,7 +18,7 @@ int main(int argc, char * argv[]) {
     struct timeval timeout;
     char header[12];
     int response;
-    int read_header;
+    int reader;
     char c[1];
     char block[4];
 
@@ -51,6 +51,10 @@ int main(int argc, char * argv[]) {
     /* get host IP address  */
     /* Hint: use gethostbyname() */
     host = gethostbyname(server_name);
+    if (host == NULL){
+      fprintf(stderr, "host not found\n");
+      exit(-1);
+    }
 
     /* set address */
     memset(&address, 0, sizeof(address));
@@ -59,7 +63,11 @@ int main(int argc, char * argv[]) {
     memcpy(&address.sin_addr.s_addr, host->h_addr, host->h_length);
 
     /* connect to the server socket */
-    minet_connect(fd, (struct sockaddr_in *)&address < 0);
+    minet_connect(fd, (struct sockaddr_in *)&address);
+    if (minet_connect < 0){
+      fprintf(stderr, "connect failed\n");
+      exit(-1);
+    }
 
     /* send request message */
     if (server_path[0] == '/')
@@ -79,34 +87,42 @@ int main(int argc, char * argv[]) {
 
     /* first read loop -- read headers */
     minet_read(fd, header, 12);
+    if (minet_read <= 0){
+      fprintf(stderr, "couldn't read header");
+      exit(-1);
+    }
     response = atoi(header + 9);
 
     /* examine return code */
     if (response == 200){     // Normal reply has return code 200
       do{
-        read_header = read(fd, c, 1);
+        reader = read(fd, c, 1);
 
-        if (*c == '/r'){
+        if (*c == '\r'){
           minet_read(fd, block, 3);
+          if (minet_read < 0){
+            fprintf(stderr, "Could not read block in header");
+            exit(-1);
+          }
           block[3] = '\0';
 
           if (strcmp(block, "\n\r\n" == 0))
             break;
         }
-      } while(read_header > 0);
+      } while(reader > 0);
       do {
-        read_header = minet_read(fd, c, 1);
-        if (read_header > 0)
+        reader = minet_read(fd, c, 1);
+        if (reader > 0)
           printf("%c", c[0]);
-      } while(read_header > 0);
+      } while(reader > 0);
     }
     else {
       printf("%s", header);
       do{
-        read_header = minet_read(fd, c, 1);
-        if (read_header > 0)
+        reader = minet_read(fd, c, 1);
+        if (read > 0)
           printf("%c", c[0]);
-      } while(read_header > 0);
+      } while(reader > 0);
     }
 
     minet_close(fd);
