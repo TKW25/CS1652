fe57fb19112d4baef527b11d334072997ce64bae
Robert Colleran <rjc59@pitt.edu>
2017-09-26 23:50:17 -0400

    client fix

--- http_client.cc
+++ http_client.cc
@@ -62,7 +62,10 @@ int main(int argc, char * argv[]) {
     minet_connect(fd, (struct sockaddr_in *)&address < 0);
 
     /* send request message */
-    sprintf(req, "GET %s HTTP/1.0\r\n\r\n", server_path);
+    if (server_path[0] == '/')
+      sprintf(req, "GET %s HTTP/1.0\r\n\r\n", server_path);
+    else
+      sprintf(req, "GET /%s HTTP/1.0\r\n\r\n", server_path);
     minet_write(fd, req, strlen(req));
 
     timeout.tv_sec = 10;
